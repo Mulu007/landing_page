4b9ebf87740c3bbfa8b0502ed2456b2326e22fb1
Mulu <brianmulu007@gmail.com>
2023-06-04 16:39:26 +0200

    Fixing of sidebar menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 
   return (
     <div className='flex justify-between  items-center h-44 max-w-[1240px] mx-auto px-4'>
-      <h1 className='w-full text-3xl font-bold text-[#1e40af]'>WATERFORD KHAMLABA</h1>
+      <h1 className='w-full text-3xl font-bold text-[#1e40af]'>WATERFORD KAMHLABA</h1>
       {/* <a href="https://flowbite.com/" >
         <img src={Data.image5} className='h-auto left-0 w-[250px]' alt="UWC Logo" />
     </a> */}
@@ -28,7 +28,7 @@ const Navbar = () => {
         <div onClick={handleNav} className="block md:hidden ">
           {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
         </div>  
-        <div className={nav ? "fixed left-0 top-0 w-[60%] h-full border-r-gray-900 bg-slate-300 ease-in-out duration-500 " : "ease-in-out duration-500 fixed left-[-100%]"}>
+        <div className={nav ? "fixed left-0 top-0 w-[60%] h-full border-r-gray-900 bg-slate-300 ease-in-out duration-500 z-10" : "ease-in-out duration-500 fixed left-[-100%]"}>
         <h1 className='w-full text-3xl font-bold text-[#1e40af] m-4 '>WATERFORD KAMHLABA</h1>
           <ul className='uppercase p-4'>
             <li className='p-4 border-b border-gray-600'>Home</li>
