ad2e288fce2c82a3ec889db3a6a13712b6c0377e
Mulu <brianmulu007@gmail.com>
2024-02-23 14:02:24 +0200

    logo addition

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import {AiOutlineClose, AiOutlineMenu} from "react-icons/ai"
-// import { Data } from './Data'
 
 const Navbar = () => {
   const [nav, setNav] = useState(false)
@@ -10,14 +9,9 @@ const Navbar = () => {
   }
 
   return (
-    <div className='flex justify-between  items-center h-44 max-w-[1240px] mx-auto px-4'>
-      <h1 className='w-full text-3xl font-bold text-[#1e40af]'>WATERFORD KAMHLABA</h1>
-      {/* <a href="https://flowbite.com/" >
-        <img src={Data.image5} className='h-auto left-0 w-[250px]' alt="UWC Logo" />
-    </a> */}
+    <div className='flex justify-between items-center h-44 max-w-[1240px] mx-auto px-4'>
+      <img className="object-cover relative  border border-gray-100 shadow-xl max-w-xl h-auto" src='https://www.waterford.sz/logos/WKUWCSALogo.png'/>
 
-      {/* <img className= "object-cover relative rounded-full border border-gray-100 shadow-sm max-w-max h-full src = "" /> */}
-      {/* <img className="object-cover relative rounded-full border border-gray-100 shadow-sm max-w-max h-full" src='https://www.waterford.sz/'/> */}
       <ul className='hidden md:flex cursor-pointer '>
         <li className='p-4 '>Home</li>
         <li className='p-4'>Company</li>
@@ -25,8 +19,10 @@ const Navbar = () => {
         <li className='p-4'>About</li>
         <li className='p-4'>Contact</li>
       </ul>
+
+                  {/* Hamburger menu */}
         <div onClick={handleNav} className="block md:hidden ">
-          {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
+          {nav ? <AiOutlineClose size={30}/> : <AiOutlineMenu size={30}/>}
         </div>  
         <div className={nav ? "fixed left-0 top-0 w-[60%] h-full border-r-gray-900 bg-slate-300 ease-in-out duration-500 z-10" : "ease-in-out duration-500 fixed left-[-100%]"}>
         <h1 className='w-full text-3xl font-bold text-[#1e40af] m-4 '>WATERFORD KAMHLABA</h1>
